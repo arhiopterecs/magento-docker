25d62f3a64c48e20439a8109738f3df2f3bab272
Yevhen Miroshnychenko <miroshny@adobe.com>
2021-08-17 13:09:26 -0500

    reserv

--- monitor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source ./.env
-
-TIME=${1}
-
-watch -n ${TIME} "docker-compose ps && mutagen sync list --label-selector=project=${PROJECT_NAME} | grep -E '(Name|Identifier|Connection|Status|Conflicts)'"
-
